4b3837c5a3c0edb9d2a09ea663c5fce518683287
animalia <moluapple@gmail.com>
2012-03-25 14:50:56 +0800

    run jsx with sublime

---
+++ python27 version/targetIllustrator.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+import sys
+from win32com.client import Dispatch
+
+def main(f):
+    app = Dispatch('Illustrator.Application.CS5')
+    result = app.DoJavaScriptFile(f) # 执行并返回结果
+    print unicode(result).encode('utf-8') # 输出结果至console
+
+if __name__ == '__main__':
+    main(sys.argv[1])
